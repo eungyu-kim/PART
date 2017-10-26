777659fa3493b90520ad9332181ef37fd2867d4a
TaeHwanL <soslth@naver.com>
2017-10-27 04:09:10 +0900

    회원가입 DB구축

--- app/src/main/java/io/github/hidroh/calendar/RegisterActivity.java
+++ app/src/main/java/io/github/hidroh/calendar/RegisterActivity.java
@@ -2,10 +2,22 @@ package io.github.hidroh.calendar;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 /**
  * Created by soslt on 2017-10-26.
@@ -15,6 +27,13 @@ public class RegisterActivity extends AppCompatActivity {
 
     private ArrayAdapter adapter;
     private Spinner spinner;
+    private String userID;
+    private String userPassword;
+    private String userGender;
+    private String userMajor;
+    private String userEmail;
+    private AlertDialog dialog;
+    private boolean validate = false;
 
     public RegisterActivity() {
     }
@@ -34,6 +53,129 @@ public class RegisterActivity extends AppCompatActivity {
         Typeface typeface = Typeface.createFromAsset(getAssets(), "NanumPen.ttf");
         Title.setTypeface(typeface);
         Title.append("회원 가입");
+
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final EditText emailText = (EditText) findViewById(R.id.emailText);
+
+        RadioGroup genderGroup = (RadioGroup) findViewById(R.id.genderGroup);
+        int genderGroupID = genderGroup.getCheckedRadioButtonId();
+        userGender = ((RadioButton) findViewById(genderGroupID)).getText().toString();
+
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton genderButton = (RadioButton) findViewById(i);
+                userGender = genderButton.getText().toString();
+            }
+        });
+
+        final Button validateButton = (Button) findViewById(R.id.validateButton);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = idText.getText().toString();
+                if (validate){
+                    return;
+                }
+                if (userID.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디를 입력해주세요.").setPositiveButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("사용할 수 있는 아이디입니다.").setPositiveButton("확인", null).create();
+                                dialog.show();
+                                idText.setEnabled(false);
+                                validate = true;
+                                idText.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                                validateButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("사용할 수 없는 아이디입니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userID, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+                String userMajor = spinner.getSelectedItem().toString();
+                String userEmail = emailText.getText().toString();
+
+                if (!validate){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("중복 체크를 해주세요.").setPositiveButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                if (userID.equals("") || userPassword.equals("") || userMajor.equals("") || userEmail.equals("") || userGender.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("빈 칸 없이 입력해주세요.").setPositiveButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("회원 가입에 성공했습니다.").setPositiveButton("확인", null).create();
+                                dialog.show();
+                                finish();
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("회원 가입에 실패했습니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userGender, userMajor, userEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (dialog != null){
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
 }
---
+++ app/src/main/java/io/github/hidroh/calendar/RegisterRequest.java
@@ -0,0 +1,32 @@
+package io.github.hidroh.calendar;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by soslt on 2017-10-27.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    final static private String URL = "http://prtrip.cafe24.com/UserRegister.php";
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String userID, String userPassword, String userGender, String userMajor, String userEmail, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+        parameters.put("userGender", userGender);
+        parameters.put("userMajor", userMajor);
+        parameters.put("userEmail", userEmail);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
