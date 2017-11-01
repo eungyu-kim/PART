5230652977fdaa93bbe26634dc93ba7be652fae1
egk00 <cksk0018@naver.com>
2017-11-01 23:14:46 +0900

    관광지 세부정보 완료

---
+++ app/src/main/java/io/github/hidroh/calendar/HotelActivity.java
@@ -0,0 +1,17 @@
+package io.github.hidroh.calendar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kug00 on 2017-11-01.
+ */
+
+public class HotelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel);
+    }
+}
